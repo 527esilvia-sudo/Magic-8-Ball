1e469bd7254cc9dd88ed74f495ff15ca8dc34942
527esilvia-sudo <527Esilvia@frhsd.com>
2025-12-01 10:40:34 -0500

    1st day

---
+++ 8Ball.js
@@ -0,0 +1,37 @@
+function shakeMagic8Ball(){
+    const question = prompt("What would you like to ask the Magic 8 Ball?");
+
+    if(question === null){
+        document.getElementById('response-text').innerText = "Please input a valid question...";
+    }
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
